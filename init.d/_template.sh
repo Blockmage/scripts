125d93f2533ed6b4e50e9bce26875687f66a515c
Alchemyst0x <alchemyst@blockmage.org>
2024-11-20 17:26:57 -0500

    refactor(template): add HERE variable to template

--- init.d/_template.sh
+++ init.d/_template.sh
@@ -1,6 +1,7 @@
 # shellcheck shell=bash
 
 [[ -n "${BASH_VERSION:-}" ]] && SELF="${BASH_SOURCE[0]}" || SELF="$0"
+HERE="$(cd "$(dirname "$SELF")" && pwd)" SELF="$HERE/$(basename "$SELF")"
 [[ "${SOURCED:=}" == *":${SELF}"* ]] && { debug_sourced && return 0; } ||
   SOURCED="${SOURCED#:}:${SELF%:}"
 
